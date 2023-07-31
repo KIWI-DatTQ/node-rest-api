b7a9aea435b9097f862a896a317cfc03a48ee089
dattrinhquoc <dattrinhquoc112@gmail.com>
2023-07-31 09:10:56 +0700

    fist commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express ();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+apiTimeout = 5 * 1000
+// app.setTimeout(10 * 1000)
+
+const delay = (t, val) => new Promise(resolve => setTimeout(resolve, t, val));
+
+app.post("/timeout", async (request, response, next) => {
+    response.setTimeout(apiTimeout, () => {
+        let err = new Error('Service Unavailable');
+        err.status = 503;
+        next(err);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
