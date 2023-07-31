0c23e4d9da8ad1f0f074d044a9eacf3dd3561bf9
dattrinhquoc <dattrinhquoc112@gmail.com>
2023-07-31 09:43:01 +0700

    Add route

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require("express");
 
-const app = express ();
+const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-apiTimeout = 5 * 1000
+apiTimeout = 5 * 1000;
 // app.setTimeout(10 * 1000)
 
-const delay = (t, val) => new Promise(resolve => setTimeout(resolve, t, val));
+const delay = (t, val) => new Promise((resolve) => setTimeout(resolve, t, val));
 
-app.post("/timeout", async (request, response, next) => {
-    response.setTimeout(apiTimeout, () => {
-        let err = new Error('Service Unavailable');
-        err.status = 503;
-        next(err);
-    });
+app.post("/api/timeout", async (request, response, next) => {
+  response.setTimeout(apiTimeout, () => {
+    let err = new Error("Service Unavailable");
+    err.status = 503;
+    next(err);
+  });
+});
+
+app.get("/api/ping", async (request, response, next) => {
+  response.send({
+    data: {
+      status: "OK",
+    },
+  });
 });
 
 app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
+  console.log("Server Listening on PORT:", PORT);
 });
